99b3e9240e76023e3ab1a1e38c4f7d8cfb36c2aa
Peace Mathew <zyrontech101@gmail.com>
2025-02-04 20:00:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -157,7 +157,7 @@
                 if (data.song) {
                     output.innerText = data.song;
                 } else {
-                    output.innerText = 'Sorry, something went wrong!';
+                    output.innerText = data.song';
                 }
             } catch (error) {
                 output.innerText = 'Error: Unable to generate a song.';
